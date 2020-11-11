1268ca70135a609143cf9e6d1ecfa74de36c0883
hayoung2 <gkdud1308@naver.com>
2020-11-11 23:07:25 +0900

    ENH : StartActivity 추가

--- app/src/main/java/with/dee2/coronavirus/StartActivity.java
+++ app/src/main/java/with/dee2/coronavirus/StartActivity.java
@@ -1,25 +1,64 @@
 package with.dee2.coronavirus;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Pair;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityOptionsCompat;
+import androidx.core.view.ViewCompat;
 
 public class StartActivity extends AppCompatActivity {
+
+    TextView maskText,creator;
+    ImageView coronaLogo;
+    LinearLayout start_background;
+    AnimationDrawable animationDrawable;
+
+    Animation top,bottom;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
+        start_background=findViewById(R.id.start_background);
+        animationDrawable=(AnimationDrawable) start_background.getBackground();
+        animationDrawable.setEnterFadeDuration(1500);
+        animationDrawable.setExitFadeDuration(1700);
+        animationDrawable.start();
+
+        creator=findViewById(R.id.creator);
+        maskText =findViewById(R.id.maskText);
+         coronaLogo=findViewById(R.id.coronaLogo);
+        top = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.top_animation);
+        bottom = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.bottom_animation);
+
         Handler mHandler = new Handler();
+
+        coronaLogo.startAnimation(top);
+        maskText.startAnimation(bottom);
+        creator.startAnimation(bottom);
+
+
         mHandler.postDelayed(new Runnable()  {
             public void run() {
                 Intent intent = new Intent(StartActivity.this, MainActivity.class);
-                startActivity(intent);
+              ActivityOptionsCompat options=ActivityOptionsCompat.makeSceneTransitionAnimation(StartActivity.this,coronaLogo, "imageTransition");
+               startActivity(intent,options.toBundle());
                 finish();
             }
-        }, 5000); // 0.5초후
+        }, 4500); //
     }
 }
