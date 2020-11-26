e7903d27e80f83517efb8d12aafe8f8ea94e80eb
hayoung2 <gkdud1308@naver.com>
2020-11-27 05:16:57 +0900

    ENH: 옥션 웹 크롤링 구현
    옥션 마스크, 손소독제에 대한 정보 수집 및 layout 구성 수정

--- app/src/main/java/with/dee2/coronavirus/MarketFragment.java
+++ app/src/main/java/with/dee2/coronavirus/MarketFragment.java
@@ -1,20 +1,160 @@
 package with.dee2.coronavirus;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+
+import java.io.IOException;
+
+import static with.dee2.coronavirus.R.id.handButton;
 
 public class MarketFragment extends Fragment {
 
+    ImageView maskButton,handButton;
+    TextView[] maskInfo=new TextView[5];
+    ImageView[] maskUrl=new ImageView[5];
+    TextView[] maskPrice=new TextView[5];
+    String[] title=new String[5];
+    String[] url=new String[5];
+    String[] price=new String[5];
+    Handler handler;
+    final Bundle bundle = new Bundle();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_market, container, false);
+
+        View v=inflater.inflate(R.layout.fragment_market, container, false);
+        maskButton=v.findViewById(R.id.maskButton);
+        handButton=v.findViewById(R.id.handButton);
+
+       for (int i=0;i<5;i++){
+           int id = getResources().getIdentifier("maskinfo"+(i+1), "id", "with.dee2.coronavirus");
+           maskInfo[i]=v.findViewById(id);
+           id = getResources().getIdentifier("maskUrl"+(i+1), "id", "with.dee2.coronavirus");
+           maskUrl[i]=v.findViewById(id);
+           id = getResources().getIdentifier("maskPrice"+(i+1), "id", "with.dee2.coronavirus");
+           maskPrice[i]=v.findViewById(id);
+        }
+
+
+       MaskCrawling();
+       handler = new Handler(){
+            @Override
+            public void handleMessage(Message msg) {
+                Bundle bundle = msg.getData();
+                int cnt=0;
+                for (String tmp: bundle.getStringArray("title")){
+                    maskInfo[cnt].setText(tmp);
+                    cnt++;
+                }
+                cnt=0;
+                for (final String tmp : bundle.getStringArray("url")){
+                    maskUrl[cnt].setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(tmp)));
+                        }
+                    });
+                    cnt++;
+                }
+                cnt=0;
+                for (String tmp : bundle.getStringArray("price")){
+                    maskPrice[cnt].setText(tmp);
+                    cnt++;
+                }
+            }
+        };
+        maskButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MaskCrawling();
+            }
+        });
+
+        handButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                HandCrawling();
+            }
+        });
+
+        return v;
     }
+
+    private void HandCrawling() {
+
+
+    }
+
+    public void MaskCrawling(){
+        new Thread(){
+            @Override
+            public void run() {
+                Document doc = null;
+                int cnt =0;
+                try {
+                    doc = Jsoup.connect("http://browse.auction.co.kr/search?keyword=%EB%A7%88%EC%8A%A4%ED%81%AC&itemno=&nickname=&frm=hometab&dom=auction&isSuggestion=No&retry=&Fwk=%EB%A7%88%EC%8A%A4%ED%81%AC&acode=SRP_SU_0100&arraycategory=&encKeyword=%EB%A7%88%EC%8A%A4%ED%81%AC").get();
+                    Elements urls = doc.select(".link--itemcard");
+                    Elements titles = doc.select(".link--itemcard .text--title");
+                    Elements prices =doc.select(".price_seller .text--price_seller");
+                    for (Element e: titles) {
+                        Log.d(" 확인 : ",e.attr("href"));
+                        title[cnt]=e.text();
+                        cnt++;
+                        if (cnt >5){
+                            break;
+                        }
+                    }
+                    cnt=0;
+                    for (Element e: urls) {
+                        url[cnt]=e.attr("href");
+                        cnt++;
+                        if (cnt >5){
+                            break;
+                        }
+                    }
+                    cnt=0;
+                    for (Element e: prices) {
+                        price[cnt]=e.text() +" 원";
+                        cnt++;
+                        if (cnt >5){
+                            break;
+                        }
+                    }
+
+
+                    bundle.putStringArray("title", title);
+                    bundle.putStringArray("url",url);
+                    bundle.putStringArray("price",price);
+                    Message msg = handler.obtainMessage();
+                    msg.setData(bundle);
+                    handler.sendMessage(msg);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+
+    }
+
 }
